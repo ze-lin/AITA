b9f7a61197e10943ac7e1e12ff36d445541342b4
Sun Haoran <ludanxer@gmail.com>
2018-12-19 16:45:28 +0800

    finish demo

--- front-end/src/views/Course.vue
+++ front-end/src/views/Course.vue
@@ -1,18 +1,137 @@
 <template>
   <div class="course">
-    <h1>打开摄像头</h1>
-    <h1>调用 API 获取数据并展示</h1>
+    <div class="container">
+      <div class="video-container">
+        <h1>摄像头实时获取人脸内容</h1>
+        <video autoplay="true" id="videoElement"></video>
+      </div>
+      <div class="result">
+        <el-progress :text-inside="true" :stroke-width="25" :percentage="emotion.anger" color="rgb(128, 128, 128, 0.7)"></el-progress>
+        <el-progress :text-inside="true" :stroke-width="25" :percentage="emotion.disgust" color="rgb(165, 42, 42, 0.7)"></el-progress>
+        <el-progress :text-inside="true" :stroke-width="25" :percentage="emotion.fear" color="rgb(255, 215, 0, 0.7)"></el-progress>
+        <el-progress :text-inside="true" :stroke-width="25" :percentage="emotion.happiness" color="rgb(244, 164, 96, 0.7)"></el-progress>
+        <el-progress :text-inside="true" :stroke-width="25" :percentage="emotion.neutral" color="rgb(34, 139, 34, 0.7)"></el-progress>
+        <el-progress :text-inside="true" :stroke-width="25" :percentage="emotion.sadness" color="rgb(0, 139, 139, 0.7)"></el-progress>
+        <el-progress :text-inside="true" :stroke-width="25" :percentage="emotion.surprise" color="rgb(147, 112, 219, 0.7)"></el-progress>
+      </div>
+      <div class="title">
+        <p>Anger</p>
+        <p>Disgust</p>
+        <p>Fear</p>
+        <p>Happiness</p>
+        <p>Neutral</p>
+        <p>Sadness</p>
+        <p>Surprise</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  
+  data(){
+    return {
+      emotion: {
+        anger: 10,
+        disgust: 20,
+        fear: 30,
+        happiness: 40,
+        neutral: 50,
+        sadness: 60,
+        surprise: 70
+      }
+    }
+  },
+  mounted: function(){
+    let video = document.querySelector("#videoElement");
+    let obj = this;
+ 
+    if (navigator.mediaDevices.getUserMedia) {       
+        navigator.mediaDevices.getUserMedia({video: true})
+      .then(function(stream) {
+        video.srcObject = stream;
+      })
+      .catch(function() {
+        obj.$message.error('糟糕，出错了！');
+      });
+    }
+    setInterval(this.getEmotionData, 1000);
+  },
+  methods: {
+    getEmotionData: function(){
+      let obj = this;
+      var params = new URLSearchParams();
+      params.append('api_key', 'Os99MvSXhTAg7Ly4lvs34gZsTZgXBumH');
+      params.append('api_secret', 'hyHyuopDC-qm94LIg7DzrRlPDHv5KCto');
+      params.append('image_base64', this.take_image());
+      params.append('return_attributes', 'emotion');
+
+      axios({
+        method: 'post',
+        url: 'https://api-cn.faceplusplus.com/facepp/v3/detect',
+        data: params,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      })
+      .then(function (response) {
+        obj.emotion = response.data['faces'][0]['attributes']['emotion'];
+      })
+      .catch(function() {
+        obj.$message.error('糟糕，哪里出了点问题！');
+      });
+    },
+    take_image: function(){
+      let canvas = document.createElement('canvas');
+      let video = document.getElementById('videoElement');
+      let scale = 0.25;
+      canvas.width = video.videoWidth * scale;
+      canvas.height = video.videoHeight * scale;
+
+      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+      let data = canvas.toDataURL();
+      return data;
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.title{
+  width: 7%;
+  float: right;
+  margin-top: 8%;
+}
+.title p{
+  margin: 0;
+  height: 45px;
+  font-weight: 400;
+}
+.container{
+  margin-top: 3%;
+}
+.video-container {
+  height: 80%;
+  width: 40%;
+  float: left;
+  margin-left: 6%;
+  margin-top: 2%;
+}
+video {
+  height: 50%;
+  width: 100%;
+}
+.result{
+  margin-top: 8%;
+  width: 35%;
+  height: 50%;
+  float: right;
+  margin-right: 8%;
+}
+.el-progress{
+  margin-top: 20px;
+  margin-right: 5%;
+}
 </style>
 
 
--- front-end/src/views/Personal.vue
+++ front-end/src/views/Personal.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="personal">
-    <el-table :data="classes" stripe style="width: 100%">
-      <el-table-column prop="title" sortable label="课程名称" width="120"></el-table-column>
-      <el-table-column prop="teacher" sortable label="任课教师" width="120"></el-table-column>
-      <el-table-column prop="date" sortable label="开课时间" width="120"></el-table-column>
-      <el-table-column prop="time" sortable label="所用时间" width="120"></el-table-column>
-      <el-table-column prop="reading" sortable label="浏览人数" width="120"></el-table-column>
-      <el-table-column prop="genre" sortable label="分类" width="120"></el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button @click="see(scope.row)" type="text" size="small">查看</el-button>
-          <el-button @click="handleClick(scope.row)" type="text" size="small">移除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div class="table">
+      <el-card class="box-card" shadow="always">
+        <div slot="header" class="clearfix">
+          <span class="text title">用户信息</span>
+        </div>
+        <div class="text item"><b>用户名：</b>ludanxer</div>
+        <div class="text item"><b>密码：</b>*****</div>
+        <div class="text item"><b>注册时间：</b>2018-10-1</div>
+        <div class="text item"><b>用户身份：</b>学生</div>
+      </el-card>
+    </div>
+    <div class="table">
+      <el-table :data="classes" stripe style="width: 100%">
+        <el-table-column prop="title" sortable label="课程名称" width="120"></el-table-column>
+        <el-table-column prop="teacher" sortable label="任课教师" width="120"></el-table-column>
+        <el-table-column prop="date" sortable label="开课时间" width="120"></el-table-column>
+        <el-table-column prop="time" sortable label="所用时间" width="120"></el-table-column>
+        <el-table-column prop="reading" sortable label="浏览人数" width="120"></el-table-column>
+        <el-table-column prop="genre" sortable label="分类" width="120"></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button @click="see(scope.row)" type="text" size="small">查看</el-button>
+            <el-button @click="handleClick(scope.row)" type="text" size="small">移除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -54,11 +67,18 @@ export default {
 </script>
 
 <style scoped>
-.home{
+.table{
   width: 58%;
   text-align: center;
   margin-left: 20%;
   margin-right: 20%;
   margin-top: 2%;
+}
+.text{
+  display: block;
+  text-align: left;
+  font-size: 1em;
+  height: 40px;
+  line-height: 40px;
 }
 </style>
