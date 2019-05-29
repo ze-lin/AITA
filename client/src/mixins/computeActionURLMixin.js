db5c66654255ec16bf2ca2eb634a6bf05ce7c512
ludanxer <ludanxer@gmail.com>
2019-05-29 22:15:02 +0800

    delete distracting front-end files

--- client/src/mixins/computeActionURLMixin.js
+++
@@ -1,7 +0,0 @@
-export default {
-  methods: {
-    computeActionURL: function(data){
-      return process.env.VUE_APP_API_URL + data;
-    }
-  }
-};
--- client/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  publicPath: './',
-  outputDir: '../aita/templates',
-  assetsDir: process.env.NODE_ENV === 'production' ? '../static' : ''
-}
