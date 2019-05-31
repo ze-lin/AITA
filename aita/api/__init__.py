ec8daf4d5de0e2e84dab860833a37b4bcc00c866
ludanxer <ludanxer@gmail.com>
2019-05-31 08:19:03 +0800

    delete useless python files

--- aita/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import focus, course, usr, comment, completeness
-
-def init_app(app):
-    app.register_blueprint(focus.bp)
-    app.register_blueprint(course.bp)
-    app.register_blueprint(usr.bp)
-    app.register_blueprint(comment.bp)
-    app.register_blueprint(completeness.bp)
--- run.py
+++
@@ -1,6 +0,0 @@
-import aita
-
-if __name__ == "__main__":
-    # Development, Testing, Production
-    app = aita.create_app('Development') 
-    app.run()
